ecd3da7ba78e385dfa3813363b0522f41ec30403
Jacob Morgan <jake2002.jm@gmail.com>
2022-06-21 21:12:00 +0100

    First Commit :sparkles:

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Temp Website</h1>
+    <p>Check out my <a href="https://linktr.ee/jacobwmorgan">linktree</a> for more</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
